12cd256dd66c93a953eed78111147d3e323ad5bb
APT Jack Richardson <jack.richardson@aptsolutions.net>
2023-09-22 17:30:51 +0100

    Added styling + content

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
     './index.html'
   ],
   theme: {
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.333rem',
+      '2xl': '1.777rem',
+      '3xl': '2.369rem',
+      '4xl': '3.157rem',
+      '5xl': '4.209rem',
+    },
     extend: {
       fontFamily: {
         sans: ['Inclusive Sans', ...defaultTheme.fontFamily.sans],
